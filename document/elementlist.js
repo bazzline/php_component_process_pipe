c8cab4e07b4687ef7f5986ca918d02f399767b80
stevleibelt <stev@leibelt.de>
2015-02-08 00:20:02 +0100

    updated dependencies

--- document/elementlist.js
+++ document/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","Net\\Bazzline\\Component\\ProcessPipe\\ExecutableException"],["c","Net\\Bazzline\\Component\\ProcessPipe\\ExecutableInterface"],["c","Net\\Bazzline\\Component\\ProcessPipe\\Pipe"],["c","Net\\Bazzline\\Component\\ProcessPipe\\PipeInterface"],["c","RuntimeException"]];
+ApiGen.elements = [["c","Net\\Bazzline\\Component\\ProcessPipe\\ExecutableException"],["c","Net\\Bazzline\\Component\\ProcessPipe\\ExecutableInterface"],["c","Net\\Bazzline\\Component\\ProcessPipe\\InvalidArgumentException"],["c","Net\\Bazzline\\Component\\ProcessPipe\\Pipe"],["c","Net\\Bazzline\\Component\\ProcessPipe\\PipeInterface"]];
--- generate_api.php
+++
@@ -1,6 +0,0 @@
-#!/bin/php
-<?php
-//clean
-shell_exec('rm -fr document/*');
-//and rebuild
-system('php vendor/bin/apigen --source source/Net/Bazzline/Component/ProcessPipe/ --destination document/ --title "Process Pipe by Bazzline"');
